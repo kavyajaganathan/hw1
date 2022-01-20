7eb8bfc7b9fed9fcb313b035f1bdae73cf550449
kavyajaganathan <kavyajaganathan@gmail.com>
2022-01-19 23:26:25 -0600

    Till Q4 done, files generated

--- starter.py
+++ starter.py
@@ -14,11 +14,11 @@ from nltk.tokenize import TreebankWordTokenizer
 import re
 import math
 from nltk.corpus import stopwords
-from collections import Counter
-from nltk import pos_tag
-#nltk.download('stopwords')
-#nltk.download('averaged_perceptron_tagger')
-#nltk.download('inaugural')
+
+
+# nltk.download('stopwords')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('inaugural')
 
 
 class my_corpus():
@@ -35,6 +35,7 @@ class my_corpus():
         lines = lines.lower()
         # replace Replace (i) years, (ii) decimals, (iii) date days, (iv) integers and
         # (v) all other numbers with <year>, <decimal>,<days>,<integer>and<other>tags in this lower text
+        regex_other = re.compile(r'(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+')
         regex_year = re.compile(r'(?:(?:18|19|20|21)[0-9]{2})')
         regex_decimal = re.compile(r'\d*\.\d+')
         regex_integer = re.compile(r'[+-]?\b[0-9]+')
@@ -42,7 +43,8 @@ class my_corpus():
         regex_date_2 = re.compile(r'<integer>\s\w+,\s<year>')
         regex_date_3 = re.compile(r'<integer>\s\w+\s\s\[o.s.\s<integer>\s\w+\s\]\s<year>')
         regex_date_4 = re.compile(r'(monday|tuesday|wednesday|thursday|friday|saturday|sunday)')
-        regex_other = re.compile(r'(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+')
+        lines = regex_other.sub("<other>", lines)
+        print("Other done")
         lines = regex_year.sub("<year>", lines)
         print("Years done")
         lines = regex_decimal.sub("<decimal>", lines)
@@ -54,8 +56,6 @@ class my_corpus():
         lines = regex_date_3.sub("<day>", lines)
         lines = regex_date_4.sub("<day>", lines)
         print("Dates done")
-        #lines = regex_other.sub("<other>", lines)
-        #print("Other done")
         # splitting data
         file_length = len(lines)
         train_len = math.floor(0.80 * file_length)
@@ -91,7 +91,7 @@ class my_corpus():
         print("Test file created")
         return (train_file, test_file, val_file)
 
-    def summary_statistics(self,train_file,test_file,val_file):
+    def summary_statistics(self, train_file, test_file, val_file):
         with open('train_corpus.txt') as f:
             lines = [line.rstrip() for line in f]
         # term-frequency dictionary creation
@@ -111,10 +111,10 @@ class my_corpus():
                 tot_no_of_unk += value
                 no_of_out_of_vocab += 1
                 to_remove.append(key)
-        #print(tot_no_of_unk)
+        # print(tot_no_of_unk)
         for k in to_remove:
             frequency_dict.pop(k)
-        #print(tot_no_of_unk)
+        # print(tot_no_of_unk)
         frequency_dict['<UNK>'] = tot_no_of_unk
         with open('test_corpus.txt') as f:
             test_lines = [line.rstrip() for line in f]
@@ -122,36 +122,36 @@ class my_corpus():
         for token in test_lines:
             if token not in frequency_dict.keys():
                 no_of_types_unk += 1
-        #print(no_of_types_unk)
+        # print(no_of_types_unk)
         no_of_stop_words = 0
         stop_words = set(stopwords.words('english'))
         for words in lines:
             if words in stop_words:
                 no_of_stop_words += 1
-        #Custom Metrics
+        # Custom Metrics
         # Custom Metrics 1:
         from nltk import pos_tag
         tokens_tag = pos_tag(lines)
         from collections import Counter
         counts = Counter(tag for word, tag in tokens_tag)
-        #print(counts)
+        # print(counts)
         # Custom Metrics 2:
         from nltk.corpus import inaugural
         total_lens = 0
         for i, sentence in enumerate(inaugural.sents(fileids='/Users/kavyajaganathan/Downloads/hw1/source_text.txt')):
             total_lens += len(sentence)
-        #print(total_lens / i)
+        # print(total_lens / i)
         print("Summary Statistics:")
         print("Number of Tokens in each split:")
-        print("Train:",len(train_file))
-        print("Validation:",len(val_file))
-        print("Test:",len(test_file))
+        print("Train:", len(train_file))
+        print("Validation:", len(val_file))
+        print("Test:", len(test_file))
         print("\n")
-        print("Vocabulary Size:",original_size)
-        print("Number of <UNK> tokens:",tot_no_of_unk)
-        print("Number of out of vocabulary words:",no_of_out_of_vocab)
-        print("Number of types mapped to UNK:",no_of_types_unk)
-        print("Number of Stop Words:",no_of_stop_words)
+        print("Vocabulary Size:", original_size)
+        print("Number of <UNK> tokens:", tot_no_of_unk)
+        print("Number of out of vocabulary words:", no_of_out_of_vocab)
+        print("Number of types mapped to UNK:", no_of_types_unk)
+        print("Number of Stop Words:", no_of_stop_words)
         print("\n")
         print("Custom Metric 1: POS Tagging:")
         for k, v in counts.items():
@@ -180,8 +180,8 @@ class my_corpus():
 
 def main():
     corpus = my_corpus(None)
-    train_corpus_file,test_corpus_file,val_corpus_file = my_corpus.corpus_construction(corpus, "source_text.txt")
-    my_corpus.summary_statistics(corpus,train_corpus_file,test_corpus_file,val_corpus_file)
+    train_corpus_file, test_corpus_file, val_corpus_file = my_corpus.corpus_construction(corpus, "source_text.txt")
+    my_corpus.summary_statistics(corpus, train_corpus_file, test_corpus_file, val_corpus_file)
     text = input('Please enter a test sequence to encode and recover: ')
     print(' ')
     ints = corpus.encode_as_ints(text)
